2bc7b9ef8c408ad26cc157fa9fa446e93a8b1c71
Rafael Khachikian <Khachikyan38@gmail.com>
2022-04-04 17:27:03 +0300

    React Router 5.3.0 version

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,7 +1,7 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
-import MarvelService from "../../services/MarvelService";
+import useMarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMassage/ErrorMessage";
 import Skeleton from "../skeleton/Skeleton";
@@ -9,79 +9,43 @@ import Skeleton from "../skeleton/Skeleton";
 import "./charInfo.scss";
 import thor from "../../resources/img/thor.jpeg";
 
-class CharInfo extends Component {
-  state = {
-    char: null,
-    loading: false,
-    error: false,
-  };
+const CharInfo = (props) => {
+  const [char, setChar] = useState(null);
 
-  marvelService = new MarvelService();
+  const { loading, error, getCharecter, clearError } = useMarvelService();
 
-  componentDidMount() {
-    this.updateChar();
-  }
+  useEffect(() => {
+    updateChar();
+  }, [props.charId]);
 
-  componentDidUpdate(prevProps) {
-    if (this.props.charId !== prevProps.charId) {
-      this.updateChar();
-    }
-  }
-
-  updateChar = () => {
-    const { charId } = this.props;
-
+  const updateChar = () => {
+    const { charId } = props;
     if (!charId) {
       return;
     }
-    this.onCharLoading();
 
-    this.marvelService
-      .getCharecter(charId)
-      .then(this.onCharLoaded)
-      .catch(this.onError);
-
-    // this.foo.bar = 0;
+    clearError();
+    getCharecter(charId).then(onCharLoaded);
   };
 
-  onCharLoaded = (char) => {
-    this.setState({
-      char,
-      loading: false,
-    });
+  const onCharLoaded = (char) => {
+    setChar(char);
   };
 
-  onCharLoading = () => {
-    this.setState({
-      loading: true,
-    });
-  };
+  const skeleteon = char || loading || error ? null : <Skeleton />;
+  const errorMessage = error ? <ErrorMessage /> : null;
+  const spinner = loading ? <Spinner /> : null;
+  const content = !(loading || error || !char) ? <View char={char} /> : null;
 
-  onError = () => {
-    this.setState({
-      loading: false,
-      error: true,
-    });
-  };
-
-  render() {
-    const { char, loading, error } = this.state;
-
-    const skeleteon = char || loading || error ? null : <Skeleton />;
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error || !char) ? <View char={char} /> : null;
-
-    return (
-      <div className="char__info">
-        {skeleteon}
-        {errorMessage}
-        {spinner}
-        {content}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="char__info">
+      {skeleteon}
+      {errorMessage}
+      {spinner}
+      {content}
+    </div>
+  );
+};
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki, comics } = char;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,36 +1,42 @@
-class MarvelService {
-  timestamp = `ts=1`;
-  hash = "hash=1da3b6bb6bfc1f6b1948dd46f7626027";
-  _apiBase = `https://gateway.marvel.com:443/v1/public/`;
-  _apiKey = "apikey=b21c527666e7784f4b1f86ec0774f8cf";
-  link = `${this._apiKey}&${this.timestamp}&${this.hash}`;
-  _baseOffset = 210;
+import { useHttp } from "../hooks/http.hook";
 
-  getResource = async (url) => {
-    let res = await fetch(url);
+const useMarvelService = () => {
+  const { loading, request, error, clearError } = useHttp();
 
-    if (!res.ok) {
-      throw new Error(`could not fetch ${url}, status:${res.status}`);
-    }
-    return await res.json();
-  };
+  const timestamp = `ts=1`;
+  const hash = "hash=1da3b6bb6bfc1f6b1948dd46f7626027";
+  const _apiBase = `https://gateway.marvel.com:443/v1/public/`;
+  const _apiKey = "apikey=b21c527666e7784f4b1f86ec0774f8cf";
+  const link = `${_apiKey}&${timestamp}&${hash}`;
+  const _baseOffset = 210;
 
-  getAllCharecters = async (offset = this._baseOffset) => {
-    const res = await this.getResource(
-      `${this._apiBase}characters?limit=9&offset=${offset}&${this.link}`
+  const getAllCharecters = async (offset = _baseOffset) => {
+    const res = await request(
+      `${_apiBase}characters?limit=9&offset=${offset}&${link}`
     );
 
-    return res.data.results.map(this._transformCharacter);
+    return res.data.results.map(_transformCharacter);
   };
 
-  getCharecter = async (id) => {
-    const res = await this.getResource(
-      `${this._apiBase}characters/${id}?${this.link}`
+  const getCharecter = async (id) => {
+    const res = await request(`${_apiBase}characters/${id}?${link}`);
+
+    return _transformCharacter(res.data.results[0]);
+  };
+
+  const getAllComics = async (offset = 0) => {
+    const res = await request(
+      `${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`
     );
-    return this._transformCharacter(res.data.results[0]);
+    return res.data.results.map(_transformComics);
   };
 
-  _transformCharacter = (char) => {
+  const getComics = async (id) => {
+    const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+    return _transformComics(res.data.results[0]);
+  };
+
+  const _transformCharacter = (char) => {
     return {
       id: char.id,
       name: char.name,
@@ -43,6 +49,30 @@ class MarvelService {
       comics: char.comics.items,
     };
   };
-}
 
-export default MarvelService;
+  const _transformComics = (comics) => {
+    return {
+      id: comics.id,
+      title: comics.title,
+      description: comics.description || "There is no description",
+      pageCount: comics.pageCount
+        ? `${comics.pageCount} p.`
+        : "No information about the number of pages",
+      thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+      language: comics.textObjects.language || "en-us",
+      price: comics.prices.price ? `${comics.prices.price}$` : "not available",
+    };
+  };
+
+  return {
+    loading,
+    error,
+    clearError,
+    getAllCharecters,
+    getCharecter,
+    getAllComics,
+    getComics,
+  };
+};
+
+export default useMarvelService;
